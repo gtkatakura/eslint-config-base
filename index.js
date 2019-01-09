27ebe0127e610c6583176cec88439af0ad23eddc
gtkatakura <gt.katakura@gmail.com>
2019-01-08 22:20:27 -0200

    feat(rules): airbnb + custom configurations

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 module.exports = {
-}
+  extends: "airbnb",
+  rules: {
+    "arrow-parens": "off",
+    "import/prefer-default-export": "off",
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        max: 1,
+        maxEOF: 1
+      }
+    ],
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "object-curly-newline": "off",
+    "react/jsx-filename-extension": "off",
+    "react/no-multi-comp": "off",
+    semi: ["error", "never"]
+  }
+};
